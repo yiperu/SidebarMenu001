da1e9652f3d96c6544a15d9cee5973f3dc0a632c
Henry AT <yiperu@gmail.com>
2013-10-09 14:26:21 -0500

    Creamos una vista UITableViewController, creamos su clase y lo conectamos como clase controladora

---
+++ SidebarMenu001/SideBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  SideBarViewController.h
+//  SidebarMenu001
+//
+//  Created by Henry AT on 10/9/13.
+//  Copyright (c) 2013 Henry AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SideBarViewController : UITableViewController
+
+@end
