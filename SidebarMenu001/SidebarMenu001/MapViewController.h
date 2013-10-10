4de09802835cd0dc6cab0ed1953a22dfff54723f
Henry AT <yiperu@gmail.com>
2013-10-09 19:38:25 -0500

    Creamos todos las vistas necesarias para nuestro storyBoard, mapa y fotos ocn sus respecitvas clases controladoras, pero dejamos un UIViewController para lkuego asignare con una clase especial

---
+++ SidebarMenu001/SidebarMenu001/MapViewController.h
@@ -0,0 +1,17 @@
+//
+//  MapViewController.h
+//  SidebarMenu001
+//
+//  Created by Henry AT on 10/9/13.
+//  Copyright (c) 2013 Henry AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
+
+
+@end
---
+++ SidebarMenu001/SidebarMenu001/PhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoViewController.h
+//  SidebarMenu001
+//
+//  Created by Henry AT on 10/9/13.
+//  Copyright (c) 2013 Henry AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoViewController : UIViewController
+
+@end
